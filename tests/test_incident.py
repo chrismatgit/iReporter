a7436d6b088b851f22daa8c872165bc5ba4a904e
root <root@localhost.localdomain>
2018-12-04 12:27:06 +0300

    modifying test incident in purporse of adding more endpoint tests

--- tests/test_incident.py
+++ tests/test_incident.py
@@ -35,7 +35,7 @@ class TestIncident(unittest.TestCase):
         self.assertIn('incident created', reply['message'])
         self.assertEqual(response.status_code, 201)
     
-    def test_incType_is_valid(self):
+    def test_incType_is_valid_is_empty(self):
         incident = {
                     "location": 121254154,
                     "createdBy": "1",
@@ -56,7 +56,92 @@ class TestIncident(unittest.TestCase):
         self.assertIn('incType field can not be left empty or it should be eg: red-flag or intervention', reply['message'])
         self.assertEqual(response.status_code, 400)
 
-    def test_image_is_valid(self):
+    def test_incType_is_valid_red_flag_is_invalid(self):
+        incident = {
+                    "location": 121254154,
+                    "createdBy": "1",
+                    "incType": "red-fla",
+                    "comment": "still under inverstigation",
+                    "status": "under_investigation",
+                    "video": "video.mp4",
+                    "image": "image.jpg"
+                    
+                }
+        response = self.tester.post(
+            'api/v1/incident/', content_type ='application/json',
+            data=json.dumps(incident)
+        )
+        reply = json.loads(response.data.decode())
+
+        print(response.data)
+        self.assertIn('incType field can not be left empty or it should be eg: red-flag or intervention', reply['message'])
+        self.assertEqual(response.status_code, 400)
+
+    def test_incType_is_valid_intervention(self):
+        incident = {
+                    "location": 121254154,
+                    "createdBy": "1",
+                    "incType": "interventio",
+                    "comment": "still under inverstigation",
+                    "status": "under_investigation",
+                    "video": "video.mp4",
+                    "image": "image.jpg"
+                    
+                }
+        response = self.tester.post(
+            'api/v1/incident/', content_type ='application/json',
+            data=json.dumps(incident)
+        )
+        reply = json.loads(response.data.decode())
+
+        print(response.data)
+        self.assertIn('incType field can not be left empty or it should be eg: red-flag or intervention', reply['message'])
+        self.assertEqual(response.status_code, 400)
+
+    def test_incType_when_red_flag_is_valid(self):
+        incident = {
+                    "location": 121254154,
+                    "createdBy": "1",
+                    "incType": "red-flag",
+                    "comment": "still under inverstigation",
+                    "status": "under_investigation",
+                    "video": "video.mp4",
+                    "image": "image.jpg"
+                    
+                }
+        response = self.tester.post(
+            'api/v1/incident/', content_type ='application/json',
+            data=json.dumps(incident)
+        )
+        reply = json.loads(response.data.decode())
+
+        print(response.data)
+        self.assertIn('incident created', reply['message'])
+        self.assertEqual(response.status_code, 201)
+
+    def test_incType_when_intervention_is_valid(self):
+        incident = {
+                    "location": 121254154,
+                    "createdBy": "1",
+                    "incType": "intervention",
+                    "comment": "still under inverstigation",
+                    "status": "under_investigation",
+                    "video": "video.mp4",
+                    "image": "image.jpg"
+                    
+                }
+        response = self.tester.post(
+            'api/v1/incident/', content_type ='application/json',
+            data=json.dumps(incident)
+        )
+        reply = json.loads(response.data.decode())
+
+        print(response.data)
+        self.assertIn('incident created', reply['message'])
+        self.assertEqual(response.status_code, 201)
+
+
+    def test_image_is_invalid(self):
         incident = {
                     "location": 121254154,
                     "createdBy": "1",
@@ -77,7 +162,29 @@ class TestIncident(unittest.TestCase):
         self.assertIn('image field can not be left empty', reply['message'])
         self.assertEqual(response.status_code, 400)
 
-    def test_video_is_valid(self):
+    def test_image_is_valid(self):
+        incident = {
+                    "location": 121254154,
+                    "createdBy": "1",
+                    "incType": "red-flag",
+                    "comment": "still under inverstigation",
+                    "status": "under_investigation",
+                    "video": "video.mp4",
+                    "image": "image"
+                    
+                }
+        response = self.tester.post(
+            'api/v1/incident/', content_type ='application/json',
+            data=json.dumps(incident)
+        )
+        reply = json.loads(response.data.decode())
+
+        print(response.data)
+        self.assertIn('incident created', reply['message'])
+        self.assertEqual(response.status_code, 201)
+
+
+    def test_video_is_invalid(self):
         incident = {
                     "location": 121254154,
                     "createdBy": "1",
@@ -98,8 +205,29 @@ class TestIncident(unittest.TestCase):
         self.assertIn('video field can not be left empty', reply['message'])
         self.assertEqual(response.status_code, 400)
 
+    def test_video_is_valid(self):
+        incident = {
+                    "location": 121254154,
+                    "createdBy": "1",
+                    "incType": "red-flag",
+                    "comment": "still under inverstigation",
+                    "status": "under_investigation",
+                    "video": "video.mp3",
+                    "image": "image.jpg"
+                    
+                }
+        response = self.tester.post(
+            'api/v1/incident/', content_type ='application/json',
+            data=json.dumps(incident)
+        )
+        reply = json.loads(response.data.decode())
 
-    def test_comment_is_valid(self):
+        print(response.data)
+        self.assertIn('incident created', reply['message'])
+        self.assertEqual(response.status_code, 201)
+
+
+    def test_comment_is_invalid(self):
         incident = {
                     "location": 1212121,
                     "createdBy": "1",
@@ -119,6 +247,27 @@ class TestIncident(unittest.TestCase):
         print(response.data)
         self.assertIn('comment field can not be left empty', reply['message'])
         self.assertEqual(response.status_code, 400)
+
+    def test_comment_is_valid(self):
+        incident = {
+                    "location": 1212121,
+                    "createdBy": "1",
+                    "incType": "red-flag",
+                    "comment": "no comment",
+                    "status": "under_investigation",
+                    "video": "video.mp4",
+                    "image": "image.jpg"
+                    
+                }
+        response = self.tester.post(
+            'api/v1/incident/', content_type ='application/json',
+            data=json.dumps(incident)
+        )
+        reply = json.loads(response.data.decode())
+
+        print(response.data)
+        self.assertIn('incident created', reply['message'])
+        self.assertEqual(response.status_code, 201)
     
     
     def test_status_is_valid(self):
@@ -141,7 +290,177 @@ class TestIncident(unittest.TestCase):
         print(response.data)
         self.assertIn('status field can not be left empty or it should be eg: draft, resolved, under_investigation or rejected', reply['message'])
         self.assertEqual(response.status_code, 400)
-    
+
+    def test_status_draft_is_invalid(self):
+        incident = {
+                    "location": 1212121,
+                    "createdBy": "1",
+                    "incType": "red-flag",
+                    "comment": "no comment",
+                    "status": "draf",
+                    "video": "video.mp4",
+                    "image": "image.jpg"
+                    
+                }
+        response = self.tester.post(
+            'api/v1/incident/', content_type ='application/json',
+            data=json.dumps(incident)
+        )
+        reply = json.loads(response.data.decode())
+
+        print(response.data)
+        self.assertIn('status field can not be left empty or it should be eg: draft, resolved, under_investigation or rejected', reply['message'])
+        self.assertEqual(response.status_code, 400)
+
+    def test_status_resolved_is_invalid(self):
+        incident = {
+                    "location": 1212121,
+                    "createdBy": "1",
+                    "incType": "red-flag",
+                    "comment": "no comment",
+                    "status": "resolve",
+                    "video": "video.mp4",
+                    "image": "image.jpg"
+                    
+                }
+        response = self.tester.post(
+            'api/v1/incident/', content_type ='application/json',
+            data=json.dumps(incident)
+        )
+        reply = json.loads(response.data.decode())
+
+        print(response.data)
+        self.assertIn('status field can not be left empty or it should be eg: draft, resolved, under_investigation or rejected', reply['message'])
+        self.assertEqual(response.status_code, 400)
+
+    def test_status_under_investigation_is_invalid(self):
+        incident = {
+                    "location": 1212121,
+                    "createdBy": "1",
+                    "incType": "red-flag",
+                    "comment": "no comment",
+                    "status": "under",
+                    "video": "video.mp4",
+                    "image": "image.jpg"
+                    
+                }
+        response = self.tester.post(
+            'api/v1/incident/', content_type ='application/json',
+            data=json.dumps(incident)
+        )
+        reply = json.loads(response.data.decode())
+
+        print(response.data)
+        self.assertIn('status field can not be left empty or it should be eg: draft, resolved, under_investigation or rejected', reply['message'])
+        self.assertEqual(response.status_code, 400)
+
+    def test_status_rejected_is_invalid(self):
+        incident = {
+                    "location": 1212121,
+                    "createdBy": "1",
+                    "incType": "red-flag",
+                    "comment": "no comment",
+                    "status": "rejecte",
+                    "video": "video.mp4",
+                    "image": "image.jpg"
+                    
+                }
+        response = self.tester.post(
+            'api/v1/incident/', content_type ='application/json',
+            data=json.dumps(incident)
+        )
+        reply = json.loads(response.data.decode())
+
+        print(response.data)
+        self.assertIn('status field can not be left empty or it should be eg: draft, resolved, under_investigation or rejected', reply['message'])
+        self.assertEqual(response.status_code, 400)
+
+    def test_status_draft_is_valid(self):
+        incident = {
+                    "location": 1212121,
+                    "createdBy": "1",
+                    "incType": "red-flag",
+                    "comment": "no comment",
+                    "status": "draft",
+                    "video": "video.mp4",
+                    "image": "image.jpg"
+                    
+                }
+        response = self.tester.post(
+            'api/v1/incident/', content_type ='application/json',
+            data=json.dumps(incident)
+        )
+        reply = json.loads(response.data.decode())
+
+        print(response.data)
+        self.assertIn('incident created', reply['message'])
+        self.assertEqual(response.status_code, 201)
+
+    def test_status_rejected_is_valid(self):
+        incident = {
+                    "location": 1212121,
+                    "createdBy": "1",
+                    "incType": "red-flag",
+                    "comment": "no comment",
+                    "status": "rejected",
+                    "video": "video.mp4",
+                    "image": "image.jpg"
+                    
+                }
+        response = self.tester.post(
+            'api/v1/incident/', content_type ='application/json',
+            data=json.dumps(incident)
+        )
+        reply = json.loads(response.data.decode())
+
+        print(response.data)
+        self.assertIn('incident created', reply['message'])
+        self.assertEqual(response.status_code, 201)
+
+    def test_status_resolved_is_valid(self):
+        incident = {
+                    "location": 1212121,
+                    "createdBy": "1",
+                    "incType": "red-flag",
+                    "comment": "no comment",
+                    "status": "resolved",
+                    "video": "video.mp4",
+                    "image": "image.jpg"
+                    
+                }
+        response = self.tester.post(
+            'api/v1/incident/', content_type ='application/json',
+            data=json.dumps(incident)
+        )
+        reply = json.loads(response.data.decode())
+
+        print(response.data)
+        self.assertIn('incident created', reply['message'])
+        self.assertEqual(response.status_code, 201)
+
+
+    def test_status_under_investigation_is_valid(self):
+        incident = {
+                    "location": 1212121,
+                    "createdBy": "1",
+                    "incType": "red-flag",
+                    "comment": "no comment",
+                    "status": "under_investigation",
+                    "video": "video.mp4",
+                    "image": "image.jpg"
+                    
+                }
+        response = self.tester.post(
+            'api/v1/incident/', content_type ='application/json',
+            data=json.dumps(incident)
+        )
+        reply = json.loads(response.data.decode())
+
+        print(response.data)
+        self.assertIn('incident created', reply['message'])
+        self.assertEqual(response.status_code, 201)
+
+
     def test_view_incidents(self):
         incident = {
                     "location": "12",
@@ -218,7 +537,7 @@ class TestIncident(unittest.TestCase):
         self.assertIn('incident created', reply['message'])
         self.assertEqual(response.status_code, 201)
 
-        response = self.tester.get('/api/v1/incident/6')
+        response = self.tester.get('/api/v1/incident/61')
 
         reply = json.loads(response.data.decode())
 
