b1eedfb64e92aec284b24d956f289cbd1efa34fc
root <root@localhost.localdomain>
2019-01-12 00:26:27 +0300

    User create an incident view, controller and validation
    In this process, I have been working on the incident model, view and also incident validation.
    - Since the model is already created, now in my controller, I added a function that will allow a user to login if all the validations are passed.
    - In my view, I created an endpoint that will allow the user create an incident. I called inside the create_incident function from incident_controller.
    - In my validation, i created the class incident validation which contain the methods that validate the information provide by the user.
    
    [Delivers#162669787]

--- api/Utilities/validations.py
+++ api/Utilities/validations.py
@@ -157,3 +157,142 @@ class Login_validation:
                 'status': 400,
                 'error': 'Password field can not be left empty and should be a string'
             }
+
+class Incident_validation:
+    '''Class handles all incident validations'''
+    def __init__(self, createdBy, incType, location, status, image, video, comment):
+        self.createdBy = createdBy
+        self.incType = incType
+        self.location = location
+        self.status = status
+        self.image = image
+        self.video = video
+        self.comment = comment
+
+    def validate_createdBy(self):
+        '''Method validates the createdBy attribute.
+        :return:
+        a error message
+        '''
+        if not self.createdBy or not isinstance(self.createdBy, int):
+            return {
+                'status': 400,
+                'error': 'createdBy field can not be left empty and should be an integer'
+                }
+        
+    def validate_incType(self):
+        '''Method validates the incType attribute.
+        :return:
+        a error message
+        '''
+        if not self.incType or self.incType == "" or not self.incType == "intervention" \
+        and not self.incType == "red-flag" or not isinstance(self.incType, str):
+            return {
+                'status': 400,
+                'error': 'incType field can not be left empty, it should be eg: red-flag or intervention\
+                and should be a string'
+            }
+    
+    def validate_location(self):
+        '''Method validates the location attribute.
+        :return:
+        a error message
+        '''
+        if not self.location or self.location == "" or not isinstance(self.location, str):
+            return {
+                'status': 400,
+                'error': 'location field can not be left empty and should be a string'
+            }
+    
+    def validate_status(self):
+        '''Method validates the status attribute.
+        :return:
+        a error message
+        '''
+        if not self.status or self.status == "" or not self.status == "draft" \
+        and not self.status == "under_investigation" and not self.status== "rejected"\
+         and not self.status=="resolved" or not isinstance(self.status, str):
+            return {
+                'status': 400,
+                'error':'status field can not be left empty, it should be eg: draft, resolved, under_investigation or rejected \
+                and should be a string'
+            }
+
+    def validate_image(self):
+        '''Method validates the image attribute.
+        :return:
+        a error message
+        '''
+        extensions = [".jpg", ".png"]
+        details = os.path.splitext(self.image)
+        if details[1] not in extensions:
+            return {
+                'status': 400,
+                'error': 'image has an invalid format(eg: image.png  or image.jpg'
+            }
+
+    def validate_video(self):
+        '''Method validates the video attribute.
+        :return:
+        a error message
+        '''
+        extensions = [".mp4", ".avi"]
+        details = os.path.splitext(self.video)
+        if details[1] not in extensions:
+            return {
+                'status': 400,
+                'error': 'video has an invalid format(eg: video.mp4  or video.avi)'
+        }
+    
+    def validate_comment(self):
+        '''Method validates the comment attribute.
+        :return:
+        a error message
+        '''
+        if not self.comment or self.comment == "" or not isinstance(self.comment, str):
+            return {
+                'status': 400,
+                'error': 'comment field can not be left empty and should be a string'
+            }
+    
+    @staticmethod
+    def empty_incident(data):
+        ''' Function enables to check if the reports list is empty
+        :param:
+        data - holds information of the reports list in the User class
+        :returns:
+        a error message.
+        '''
+        if len(data) == 0:
+            return {
+                'status': 400,
+                'error': 'There are no incident yet!'
+            }
+
+    @staticmethod
+    def validate_red_flag_comment(comment):
+        ''' Function enables to validate comment
+        :param:
+        comment - holds the comment information entered by a user before update it
+        :returns:
+        a error message.
+        '''
+        if not comment or comment == "" or not isinstance(comment, str) :
+            return {
+                'status': 400,
+                'error': 'comment field can not be left empty and should be a string'
+            }
+            
+    @staticmethod
+    def validate_red_flag_location(location):
+        ''' Function enables to validate location
+        :param:
+        location - holds the location information entered by a user before update it
+        :returns:
+        a error message.
+        '''
+        if not location or location == "" or not isinstance(location, str):
+            return {
+                'status': 400,
+                'error': 'location field can not be left empty and should be a string'
+            }
--- api/Views/routes.py
+++ api/Views/routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, Blueprint
 from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 from api.Controllers.user_controller import signup, login, promote_user_as_admin, get_all_users
+from api.Controllers.incident_controller import create_incident
 
 
 bp = Blueprint('application', __name__)
@@ -9,15 +10,6 @@ bp = Blueprint('application', __name__)
 def test_route():
     '''Function returns a welcome message'''
     return "welcome to iReporter"
-
-@bp.route('/welcome')
-@jwt_required
-def welcome():
-    username = get_jwt_identity()
-    return jsonify({
-        'status': 200,
-        'message': f'{username} thanks for using iReporter Api'
-    }), 200
 
 @bp.route('/signup/', methods=['POST'])
 def signUp():
@@ -28,22 +20,33 @@ def signUp():
 
 @bp.route('/login/', methods=['POST']) 
 def user_login():
-    response = login()
-    return response
+   '''Function allows the user to login
+    returns a success message and the token'''
+   response = login()
+   return response
 
 @bp.route('/user/promote/<int:user_id>', methods=['PATCH'])
 def promote_user(user_id):
-    response = promote_user_as_admin(user_id)
-    return response
+   response = promote_user_as_admin(user_id)
+   return response
 
 @bp.route('/users/', methods=['GET']) 
 def get_users():
+   '''Function allows the user to get all the users in the account list
+    returns a success message and the token'''
    response = get_all_users()
+   return response
+
+@bp.route('/incident/', methods=['POST'])
+def create_report():
+   '''Function adds the report to reports list
+    returns a success message and the account details'''
+   response = create_incident()
    return response
 
 @bp.errorhandler(404)
 def page_not_found(e):
-    return jsonify({
-        'issue': 'you have entered an unknown URL',
-        'message': 'Please contact us for more details'
-    })
+   return jsonify({
+      'issue': 'you have entered an unknown URL',
+      'message': 'Please contact us for more details'
+   })
